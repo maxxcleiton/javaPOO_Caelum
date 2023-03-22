c62f16bf59c9adf4b907110b377a312da368f01f
maxxcleiton <maxcleiton@live.com>
2023-03-22 11:26:19 -0300

    exercise 3.3 added and solved

--- JavaOperatorsAndVariables.java
+++ JavaOperatorsAndVariables.java
@@ -33,6 +33,25 @@ class JavaOperatorsAndVariables{
         char letter = 'a';
         System.out.println(letter);
 
-        
+        // Exercise (3.3):
+        // 1. Na empresa em que trabalhamos, há tabelas com o gasto de cada mês. Para fechar o balanço do primeiro trimestre, precisamos somar o gasto total. Sabendo que, em janeiro, foram gastos 15 mil reais, em fevereiro, 23 mil reais e, em março, 17 mil reais, faça um programa que calcule e imprima a despesa total no trimestre e a média mensal de gastos.
+
+        int januaryCost = 15000;
+        int februaryCost = 23000;
+        int marchCost = 17000;
+        int quarterYearCost;
+        int monthlyCostAverage;
+
+        // Despesa total no trimestre:
+        quarterYearCost = januaryCost + februaryCost + marchCost;
+
+        // Média mensal de gastos:
+        monthlyCostAverage = quarterYearCost/3;
+
+        // Impressão:
+        System.out.println("Quarter Year Total Cost: " + quarterYearCost);
+        System.out.println("Month's Cost Median: " + monthlyCostAverage);
+
+
     }
 }
