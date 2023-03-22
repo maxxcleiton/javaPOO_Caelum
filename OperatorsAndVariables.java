63d8e97ff206f4405d130cf41da5233ad6c45015
maxxcleiton <maxcleiton@live.com>
2023-03-22 10:53:14 -0300

    new file added: OperatorAndVariables

---
+++ OperatorsAndVariables.java
@@ -0,0 +1,20 @@
+class JavaOperatorsAndVariables{
+    public static void main(String[]
+    args) {
+        // I want a name:
+        String name = "Max";
+        System.out.println(name);
+
+        // Age:
+        int birthdayYear = 1998;
+        int currentlyYear = 2023;
+        int maxAge;
+
+        maxAge = currentlyYear - birthdayYear;
+
+        System.out.println("Max's age is " + maxAge);
+        
+        System.out.println("");
+        System.out.println("");
+    }
+}
