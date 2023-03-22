154e20029d9bff0c4ce05a284a4c3b2c22c960fd
maxxcleiton <maxcleiton@live.com>
2023-03-22 10:26:27 -0300

    some changes

--- MeuPrograma.java
+++ MeuPrograma.java
@@ -1,5 +1,5 @@
 class MeuPrograma{
     public static void main(String[] args) {
-        System.out.println("My first java application");
+        System.out.println("My first java application!!\n :D");
     }
 }
