8810cbefeade0eeeaa47bc00f3c4fbf8dde2d06e
maxxcleiton <maxcleiton@live.com>
2023-03-29 17:44:55 -0300

    exercise 2 added and started

---
+++ Exercises/3.13/Exercise2.java
@@ -0,0 +1,12 @@
+class Exercicio2 {
+    public static void main(String[] args){
+        // Imprima a soma de 1 até 1000.
+        for (int n1 = 1; n1 <= 1000; n1++) {
+            int n2 = n1;
+            int nSum = 1;
+            nSum = n1 + n2;
+            System.out.println(nSum);
+        }
+
+    }
+}
