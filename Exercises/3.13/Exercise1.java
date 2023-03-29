bac2ff50305dc3fb7d74508dd0f5cfb9fbec97be
maxxcleiton <maxcleiton@live.com>
2023-03-29 16:40:13 -0300

    first exercise created and solved

---
+++ Exercises/3.13/Exercise1.java
@@ -0,0 +1,9 @@
+class Exercise1 {
+    public static void main(String[] args) {
+        // Imprima todos os números de 150 a 300
+        for (int n = 150; n <= 300; n++) {
+            System.out.println("Numbers list from 150 to 300:");
+            System.out.println(n);
+        }
+    }
+}
