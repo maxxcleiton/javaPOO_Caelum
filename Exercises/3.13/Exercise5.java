6091cd252c4e65c76371e730a33c5bbce813f0e7
maxxcleiton <maxcleiton@live.com>
2023-04-07 00:05:55 -0300

    exercise 5 added and solved, and long type doesnt made any changes

--- Exercises/3.13/Exercise5.java
+++ Exercises/3.13/Exercise5.java
@@ -1,10 +1,11 @@
-class Exercise4 {
+class Exercise5 {
     public static void main(String[] args) {
-        // Print the factorial numbers from 1 to 10.
+        // Print the factorial numbers from 1 to 10, 20, 40.
+        // And then change it from int to long
 
-        int factorial = 1;
+        long factorial = 1;
 
-        for (int n = 1; n <= 10; n++) {
+        for (int n = 1; n <= 40; n++) {
             factorial = factorial * n;
 
             System.out.println("The factorial number of " + n + " is: " + factorial);
