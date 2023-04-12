85fa17d703173d80b84822ba09a05d4a2c251945
maxxcleiton <maxcleiton@live.com>
2023-04-12 20:45:47 -0300

    separation of classes and files, and new command learned: javac *.java

--- Exercises/4.12/Conta.java
+++ Exercises/4.12/Conta.java
@@ -34,21 +34,3 @@ class Conta  {
     };
     
 };
-
-class TestaConta {
-	public static void main(String[] args) {
-		Conta minhaConta;
-		minhaConta = new Conta();
-	
-        // To withdraw money
-    minhaConta.withdraw();
-
-
-        // To deposit money
-    minhaConta.deposit();
-
-        // To return your monthly earnings
-    minhaConta.calculaRendimento();
-
-};
-};
---
+++ Exercises/4.12/TestaConta.java
@@ -0,0 +1,17 @@
+class TestaConta {
+	public static void main(String[] args) {
+		Conta minhaConta;
+		minhaConta = new Conta();
+	
+        // To withdraw money
+    minhaConta.withdraw();
+
+
+        // To deposit money
+    minhaConta.deposit();
+
+        // To return your monthly earnings
+    minhaConta.calculaRendimento();
+
+};
+};
